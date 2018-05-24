d0ce94583e337921cbd0225f2a403ebff08c4e45
greybo <greiboas@gmail.com>
2018-05-24 19:13:32 +0300

    Сделал 2 задания из 3

--- bars.py
+++ bars.py
@@ -1,16 +1,36 @@
 import json
+import sys
 
+bar_seat = []
+filepath = sys.argv[1]
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as file_handler:
+        data = json.load(file_handler)
+
+    return data
+
+def get_biggest_bar(filepath):
+    data1 = load_data(filepath)
+    for f in data1.get('features'):
+        bar_seat.append(f.get('properties').get('Attributes').get('SeatsCount'))
+    for f in data1.get('features'):
+        if f.get('properties').get('Attributes').get('SeatsCount') == max(bar_seat):
+            d = f.get('properties').get('Attributes').get('Name')
+    return "Название самого большого бара: {} Посадочных мест: {}".format(d,max(bar_seat))
 
 
-def get_biggest_bar(data):
-    pass
 
 
-def get_smallest_bar(data):
-    pass
+def get_smallest_bar(filepath):
+    data1 = load_data(filepath)
+    for f in data1.get('features'):
+        bar_seat.append(f.get('properties').get('Attributes').get('SeatsCount'))
+    for f in data1.get('features'):
+        if f.get('properties').get('Attributes').get('SeatsCount') == min(bar_seat):
+            d = f.get('properties').get('Attributes').get('Name')
+    return "Название самого маленького бара: {} Посадочных мест: {}".format(d, min(bar_seat))
+
 
 
 def get_closest_bar(data, longitude, latitude):
@@ -18,4 +38,5 @@ def get_closest_bar(data, longitude, latitude):
 
 
 if __name__ == '__main__':
-    pass
+    print(get_biggest_bar(filepath))
+    print(get_smallest_bar(filepath))
