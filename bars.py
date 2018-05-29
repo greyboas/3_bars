116e0f0b395c40081965cab78e9f32cbf1d26bad
greybo <greiboas@gmail.com>
2018-05-29 17:02:46 +0300

    Переделал все

--- bars.py
+++ bars.py
@@ -1,90 +1,77 @@
 import json
 import sys
+import argparse
+from functools import partial
 
 
 def load_data(filepath):
-    bars_seat = []
-    bars_coordinate = []
+    with open(filepath, 'r', encoding='UTF8') as file_handler:
+        return json.load(file_handler)['features']
+
+
+def get_biggest_bar(loaded_bars):
+    return max(loaded_bars,
+               key=lambda bar: bar['properties']['Attributes']['SeatsCount']
+               )
+
+
+def get_smallest_bar(loaded_bars):
+    return min(loaded_bars,
+               key=lambda bar: bar['properties']['Attributes']['SeatsCount']
+               )
+
+
+def validation_longitude_latitude(dirty_latitude, dirty_longtitude):
+    latitude = float(dirty_latitude)
+    longtitude = float(dirty_longtitude)
+    return latitude, longtitude
+
+
+def get_closest_bar(loaded_bars, longitude, latitude):
+    return min(loaded_bars,
+               key=lambda bar: (
+                       (bar['geometry']['coordinates'][0] - longitude)**2 -
+                       (bar['geometry']['coordinates'][1] - latitude)**2)
+               )
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-f', action='store',
+        dest='filepath',
+        help='Filepath with data, "bars.json" by default',
+        default='bars.json'
+    )
+    parser.add_argument(
+        '-lat', action='store',
+        dest='dirty_latitude',
+        help='You latitude. If not set, it will be generated randomly',
+        default='37'
+    )
+    parser.add_argument(
+        '-long', action='store',
+        dest='dirty_longtitude',
+        help='You longtitude. If not set it will be generated randomly',
+        default='55'
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    params = parse_arguments()
+    loaded_bars = load_data(params.filepath)
     try:
-        with open(filepath, 'r') as file_handler:
-            loaded_json = json.load(file_handler)
-    except ValueError:
-        return None
-    for bar_seat in loaded_json.get('features'):
-        bars_seat.append(bar_seat.get('properties')
-                         .get('Attributes')
-                         .get('SeatsCount'))
-        bars_coordinate.append(
-            bar_seat.get('geometry')
-                .get('coordinates'))
-    return loaded_json, bars_seat, bars_coordinate
-
-
-def get_biggest_bar(loaded_json, bars_seat):
-    for bar in loaded_json.get('features'):
-        if bar.get('properties')\
-                .get('Attributes')\
-                .get('SeatsCount') == max(bars_seat):
-            biggest_bar_name = bar.get('properties')\
-                .get('Attributes')\
-                .get('Name')
-    return biggest_bar_name, max(bars_seat)
-
-
-def get_smallest_bar(loaded_json, bars_seat):
-    for bar in loaded_json.get('features'):
-        if bar\
-                .get('properties')\
-                .get('Attributes')\
-                .get('SeatsCount') == min(bars_seat):
-            smallest_bar_name = bar\
-                .get('properties')\
-                .get('Attributes')\
-                .get('Name')
-    return smallest_bar_name, min(bars_seat)
-
-
-def validation_longitude_latitude(dirty_longitude, dirty_latitude):
-    try:
-        longitude = float(dirty_longitude)
-        latitude = float(dirty_latitude)
+        longitude, latitude = validation_longitude_latitude(
+            params.dirty_latitude,
+            params.dirty_longtitude)
     except IndexError:
         print('Not set argyment coordinate')
     except ValueError:
         print('The argument format is not valid (For example, '
-                 'longitude 37.594104911195 or latitude 55.748861154831935)')
-    return longitude, latitude
-
-
-def get_closest_bar(loaded_json, bars_coordinate):
-    delta_distance = []
-    for coordinate in bars_coordinate:
-        delta_distance.append(
-            (abs(coordinate[0]-longitude))
-            +(abs(coordinate[1]-latitude)))
-    for bar in loaded_json.get('features'):
-        if bar == loaded_json.get('features')[delta_distance
-                .index(min(delta_distance))]:
-            closest_bar_name = bar\
-                .get('properties')\
-                .get('Attributes')\
-                .get('Name')
-    return closest_bar_name, coordinate
-
-
-if __name__ == '__main__':
-    dirty_longitude = sys.argv[2]
-    dirty_latitude = sys.argv[3]
-    try:
-        filepath = sys.argv[1]
-        loaded_json, bars_seat, bars_coordinate = load_data(filepath)
-    except IndexError:
-        sys.exit('Not set argyment filepath')
-    except FileNotFoundError:
-        sys.exit('File not found')
-    longitude, latitude = validation_longitude_latitude(
-        dirty_longitude,
-        dirty_latitude)
-    print(get_biggest_bar(loaded_json, bars_seat))
-    print(get_smallest_bar(loaded_json, bars_seat))
-    print(get_closest_bar(loaded_json, bars_coordinate))
+              'longitude 37.594104911195 or latitude 55.748861154831935)')
+    print(get_biggest_bar(loaded_bars)['properties']['Attributes']['Name'])
+    print(get_smallest_bar(loaded_bars)['properties']['Attributes']['Name'])
+    print(get_closest_bar(loaded_bars, longitude, latitude)
+          ['properties']['Attributes']['Name'])
